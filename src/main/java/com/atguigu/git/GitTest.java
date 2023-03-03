caeb63e947918d70e5ee8b8a636616891167e603
老马 <3314991589@qq.com>
2023-03-03 23:47:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author malinyan
+ * @Date 2023/3/3
+ * @Time 23:44
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
